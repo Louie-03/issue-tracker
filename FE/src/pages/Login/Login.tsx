8b9a5e60e968c22600ed3ad71741d8304465da40
suuzin <8oozin@gmail.com>
2022-06-15 18:03:21 +0900

    feat: 버튼재사용기능추가

--- FE/src/pages/Login/Login.tsx
+++ FE/src/pages/Login/Login.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 
+import * as S from './Login.styled';
+
 function Login(): JSX.Element {
-  return <div />;
+  return (
+    <S.Container>
+      <S.Wrapper>
+        <S.Logo>
+          <S.LogoImg />
+        </S.Logo>
+        {/* <GitHubBtn to="/" color="primary">
+          GitHub 계정으로 로그인
+        </GitHubBtn> */}
+      </S.Wrapper>
+    </S.Container>
+  );
 }
 export default Login;
